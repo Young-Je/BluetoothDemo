c34c221ebe50e18087084bee5a796863ed47e37a
shenhark <hark@shenharkdeMac.local>
2015-04-08 10:49:06 +0800

    Initial Commit

---
+++ ble sps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ble sps
+//
+//  Created by shenhark on 15/4/8.
+//  Copyright (c) 2015年 shenhark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
